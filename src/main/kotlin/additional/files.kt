8a7dbc2a462f4eb1b13985c98201e3865ae27403
smokewolfy <smokewolf@yandex.ru>
2024-06-24 08:56:49 +0300

    KTB-04

--- src/main/kotlin/additional/files.kt
+++ src/main/kotlin/additional/files.kt
@@ -2,7 +2,18 @@ package additional
 
 import java.io.File
 
+data class Word(
+    val eng: String,
+    val rus: String,
+    var correctAnswerCount: Int = 0,
+)
+
 fun main() {
     val file = File("words.txt")
-    file.readLines().forEach { println(it) }
+    val dictionary: MutableList<Word> = mutableListOf()
+    file.readLines().forEach {
+        val line = it.split("|")
+        dictionary.add(Word(eng = line[0], rus = line[1], correctAnswerCount = line[2].toIntOrNull() ?: 0))
+    }
+    dictionary.forEach { println("${it.eng} - ${it.rus}. Отвечено верно - ${it.correctAnswerCount} раз ") }
 }
